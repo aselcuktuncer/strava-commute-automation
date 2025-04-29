55dfedfc92cfbf67cce25ee4165c67204982598c
Ahmet Selçuk Tunçer <aselcuktuncer@yahoo.com>
2025-04-30 01:10:05 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const run = async () => {
 
   await updateActivities(token);
 
-  console.log("CRON job completed successfully.");
+  console.log("Cron job completed successfully.");
 };
 
 run().catch((err) => {
